dd276786f026b6e2162308ffff74cf348786b463
Massimiliano Casadei <massimiliano.casadei86@gmail.com>
2012-08-20 17:46:18 +0200

    Apertura del picker al click del carica la foto

--- iPhone/B2B/www/app/controller/Camera.js
+++ iPhone/B2B/www/app/controller/Camera.js
@@ -1,7 +1,9 @@
 Ext.define("B2B.controller.Camera", {
 	extend : "Ext.app.Controller",
+	xtype : "camera",
 	config : {
 		refs : {
+			app: "_app",
 			activitylistcontainerpanel : "cameracontainerpanel"
 		},
 		control : {
@@ -11,23 +13,41 @@ Ext.define("B2B.controller.Camera", {
 		}
 	},
 	takePhoto : function() {
-		var isDevice = (Ext.browser.is.WebView && (Ext.os.is.Android || Ext.os.is.iOS));
+		var isDevice = (Ext.browser.is.WebView && (Ext.os.is.Android || Ext.os.is.iOS)),
+			isCameraAvailable = false,
+			camera = Ext.device.Camera;
 
 		if (isDevice) {
-			Ext.device.Camera.capture({
+			camera.capture({
+				success : function(image) {
+					isCameraAvailable = true;
+				},
+				source : 'camera'
+			});
+
+			/*camera.capture({
 				success : function(image) {
 					var view = Ext.getCmp('photoView');
-					console.log(image);
 					view.setSrc(image);
+				},
+				failure : function(e) {
+					console.log("SKIFO|||" + e);
 				},
 				quality : 75,
 				width : 200,
 				height : 200,
 				destination : 'file',
-				source : 'library'
+				source : 'camera'
+			});
+			*/
+			this.getApp().push({
+				xtype: "camerapicker"
 			});
 		} else {
-			Ext.Msg.alert(i18n.app.COMMON_ATTENTION, i18n.app.LABEL_CAMERA_NOT_AVAILABLE);
+			this.getApp().push({
+			xtype: "camerapicker"
+		});
+			//Ext.Msg.alert(i18n.app.COMMON_ATTENTION, i18n.app.LABEL_CAMERA_NOT_AVAILABLE);
 		}
 	},
 	launch : function() {
