4ad3510c6effee1d47ffd29aeac54e737871507a
Hoang C. Huynh <hoangchau.huynh@gmail.com>
2012-08-20 12:47:09 +0200

    Fix per il test di camera

--- iPhone/B2B/www/app/view/Camera_Container.js
+++ iPhone/B2B/www/app/view/Camera_Container.js
@@ -1,24 +1,27 @@
 Ext.define('B2B.view.Camera_Container', {
 	extend: 'Ext.Container',
 	xtype: 'cameracontainerpanel',
-
 	config: {
 		title : i18n.app.LABEL_CAMERA,
 		iconCls : 'photo1',
 		styleHtmlContent: true,
 		scrollable: true,
 		items : [
-			{
+			
+		]
+	},
+	initialize: function (a, b) {
+		var btn_camera = {
 				xtype: "button",
 				text: i18n.app.BTN_CAMERA,
 				ui: 'action',
 				id: 'camera_btn',
 				handler: this.onCameraButtonTap,
 				scope: this
-			}
-		]
-	},
+			};
 
+		this.add([ btn_camera]);
+	},
 	onCameraButtonTap: function(){
 		console.log("CIAO");
 		this.fireEvent("cameraCommand", this);
