b28e5a3aaebef1451f675e9769df0b24a49524e8
Gabriele Morlini <gmorlini@imolinfo.it>
2012-08-28 09:11:49 +0200

    update model drink

--- iPhone/B2B/www/app/model/Drink.js
+++ iPhone/B2B/www/app/model/Drink.js
@@ -11,8 +11,10 @@ Ext.define("B2B.model.Drink", {
 			{ name: 'beerName', type: 'string' },
 			{ name: 'idPlace', type: 'string' },
 			{ name: 'placeName', type: 'string' },
-			{ name: 'idFeedback', type: 'string' },
 			{ name: 'image', type: 'string' },
+			{ name: 'rate', type: 'int' },
+			{ name: 'rate2', type: 'int' },
+			{ name: 'rate3', type: 'int' },
 			{ name: 'insertedOn', type: 'date', dateformat: 'c' }
 		],
 		validations: [
