6ef18301590ab1465e21bc5656d81289354c42bd
Hoang Chau Huynh <hoangchau.huynh@gmail.com>
2012-09-03 22:34:22 +0200

    Seriously, anche il gioco adesso?

--- iPhone/B2B/www/app/view/View_BottleSpinner.js
+++ iPhone/B2B/www/app/view/View_BottleSpinner.js
@@ -6,15 +6,20 @@ Ext.define('B2B.view.View_BottleSpinner', {
 		iconCls: 'drink_beerbottle',
 		styleHtmlContent: true,
        // cls: 'slidableToolbar',
+       layout: {type:'fit',align:'stretch'},
+       items: [
+            {
+                xtype: 'container',
+                id: 'bottlegame',
+                flex:1,
+                html: [
+                    '<div id="rules" >',
+                        '<p>RULES ARE EASY</p>',
+                    '</div>',
+                ].join("")
+            }
+       ]
 
-		html: [
-			'<div id="game" style="display: block; ">',
-                '<p id="old"></p>',
-                '<div id="shadow"> </div>',
-                '<div id="bottle" class="normal"> </div>',
-                '<div id="spintext" class="spinin"></div>',
-            '</div>',
-		].join("")
 	},
 	initialize: function(){
         
@@ -33,98 +38,83 @@ Ext.define('B2B.view.View_BottleSpinner', {
             }
         };
 
-        this.add([viewbottlespinnertoolbar]);
+        var button = {
+            xtype: 'button',
+            docked: 'bottom',
+            id: "startgamebutton",
+            label: "START",
+            height: 100,
+            handler: this.startGame
+        }
 
-        /* SPIN BOTTLE */
+        this.add([viewbottlespinnertoolbar, button]);
+    },
+    startGame: function(){
 
-        var bottle,
-            shadow,
-            spintext;
-        var timer_rot = 0; // timeout for bottle rotation
-        var rot = 0.0;   // current rotation angle, rad
-        var step;        // delta for rotation
-        var count;       // how many times repeat before change of step
-        var tdelta = 20; // timer interval
+        var playground = Ext.getCmp('bottlegame');
+        playground.setHtml([
+                    '<div id="playground" style="display: block; ">',
+                        '<div id="shadow"> </div>',
+                        '<div id="bottle" class="normal"> </div>',
+                    '</div>',
+                ].join(""));
+
+        var bottle = document.getElementById("bottle"),
+            shadow = document.getElementById("shadow");
+            what = document.getElementById("bottlegame");
+        var timer_rot = 0;
+        var rot = 0.0;
+        var step;
+        var count;
+        var tdelta = 20;
         var oldangle1 = 0;
         var oldangle2 = 0;
         var oldangle3 = 0;
         var centerx = 160;
         var centery = 150;
+        var spintimer = 1;
+        var currentWidth = 0;
+        
 
-        function main() {
-            bottle = Ext.get("bottle");
-            shadow = Ext.get("shadow");
-            spintext = Ext.get("spintext");
-
-            startspin(1000);
-            setTimeout(startTouch, 3000);
-            setInterval(updateLayout, 500);
+        what.ontouchstart = function(e) {
+            oldangle1 = 0; oldangle2 = 0; oldangle3 = 0;
         }
 
-        function startTouch() {
+        what.ontouchmove = function(e) {
+            e.preventDefault();
+            if (timer_rot != 0) return;
 
-            me.ontouchstart = function(e) {
-                oldangle1 = 0; oldangle2 = 0; oldangle3 = 0;
-                cancelspin();
-            }
+            var touch = e.touches[0];
+            var dx = centerx - touch.pageX;
+            var dy = centery - touch.pageY;
+            var angle = Math.atan2(dy, dx);
 
-            me.ontouchmove = function(e) {
+            oldangle3 = angle;
+            oldangle1 = oldangle2;
+            oldangle2 = oldangle3;
 
-                e.preventDefault();
-
-                if (timer_rot != 0)
-                    return;
-
-                var touch = e.touches[0]; // finger 1
-                var dx = centerx - touch.pageX;
-                var dy = centery - touch.pageY;
-                var angle = Math.atan2(dy, dx);
-
-                oldangle3 = angle;
-                oldangle1 = oldangle2;
-                oldangle2 = oldangle3;
-
-                var val = "rotateZ(" + angle + "rad)";
-                bottle.style.webkitTransform = val;
-                shadow.style.webkitTransform = val;
-                rot = angle;
-            }
-            me.ontouchend = function(e){
-                if (timer_rot == 0) {
-                    var val = ((oldangle2 - oldangle1) + (oldangle3 - oldangle2)) / 2;
-                    val /= 3;
-                    if (val == 0.0)
-                        val = rand(0.3)+0.2;
-                    else if (Math.abs(val) < 0.3)
-                        val *= 3;
-                    startanim(val);
+            var val = "rotateZ(" + angle + "rad)";
+            bottle.style.webkitTransform = val;
+            shadow.style.webkitTransform = val;
+            rot = angle;
+        }
+        what.ontouchend = function(e){
+            if (timer_rot == 0) {
+                var val = ((oldangle2 - oldangle1) + (oldangle3 - oldangle2)) / 2;
+                val /= 3;
+                if (val == 0.0){
+                    val = Math.random()*0.3+0.2;
                 }
+                else if (Math.abs(val) < 0.3){
+                    val *= 3;
+                }
+                count = 40;
+                step = val;
+                timer_rot = setTimeout(stepfunc, tdelta);
+                bottle.className = "normal blurred";
             }
         }
 
-        function stopTouch() {
-            me.ontouchstart = null;
-            me.ontouchmove = null;
-            me.ontouchend = null;
-        }
-
-        function elm(id) {
-            return document.getElementById(id);
-        }
-        function hide(e) {
-            elm(e).style.display = 'none';
-        }
-        function show(e) {
-            elm(e).style.display = 'block';
-        }
-
-        function startanim(startstep) {
-            count = 40;
-            step = startstep;
-            timer_rot = setTimeout(stepfunc, tdelta);
-
-            bottle.className = "normal blurred";
-        }
         function stepfunc() {
             rot += step;
             var val = "rotateZ(" + rot + "rad)";
@@ -135,57 +125,32 @@ Ext.define('B2B.view.View_BottleSpinner', {
                 count = 10;
                 step = step - step/4;
             }
-            if (Math.abs(step) < 0.003)
-                stopanim();
-            else
+            if (Math.abs(step) < 0.003){
+                bottle.className = "normal";
+                timer_rot = 0;
+            }
+            else{
                 setTimeout(stepfunc, tdelta);
+            }
         }
 
-        function stopanim() {
-            bottle.className = "normal";
-            timer_rot = 0;
-        }
-        function rand(maxnum) {
-            return Math.random()*maxnum;
-        }
-
-        var spintimer = 1;
-        function startspin(t) {
-            spintext.style.display = 'none';
-            spintimer = setTimeout(function() {
-                show("spintext");
-                spintext.className = "spinin";
-                spintimer = 0;
-            }, t);
-        }
-        function cancelspin() {
-            spintext.style.display = 'none';
-            if (spintimer != 0)
-                clearTimeout(spintimer);
-        }
-        function spinout() {
-            spintext.className = "spinout";
-        }
-
-        // detect orientation change, called periodically
-        var currentWidth = 0;
+        
         function updateLayout() {
           if (window.innerWidth != currentWidth) {
             currentWidth = window.innerWidth;
             window.scrollTo(0, 1);
-            centerbottle();
-          }
-        };
-
-        function centerbottle() {
             centerx = window.innerWidth/2;
             centery = window.innerHeight/2;
             var x = (centerx - 150);
-            var y = (centery - 50);
+            var y = (centery - 150);
             bottle.style.left = x + "px";
             bottle.style.top = y + "px";
             shadow.style.left = (x+8) + "px";
             shadow.style.top = (y+15) + "px";
-        }
+          }
+        };
+
+        updateLayout()
+        //setInterval(updateLayout, 500);
     }
 });
