6509f147abdf35cb0a83d50644f4deea862928ca
Hoang C. Huynh <hoangchau.huynh@gmail.com>
2012-09-04 10:21:27 +0200

    Aggiunto humanDate

--- iPhone/B2B/www/resources/js/hh.js
+++ iPhone/B2B/www/resources/js/hh.js
@@ -175,6 +175,20 @@ var utils = {
 			return json.username;
 		}
 	},
+	getHumanDate: function(date){
+		var seconds = Math.floor(((new Date().getTime()/1000) - date)),
+		interval = Math.floor(seconds / 31536000);
+		if (interval > 1) return interval + "y";
+		interval = Math.floor(seconds / 2592000);
+		if (interval > 1) return interval + "m";
+		interval = Math.floor(seconds / 86400);
+		if (interval >= 1) return interval + "d";
+		interval = Math.floor(seconds / 3600);
+		if (interval >= 1) return interval + "h";
+		interval = Math.floor(seconds / 60);
+		if (interval > 1) return interval + "m ";
+		return Math.floor(seconds) + "s";
+	},
 	getCountryFromCode: function(value){
 		return (_.find(i18n.countries, function(state){ return state.value === value.toUpperCase();})||"").text;
 	},
