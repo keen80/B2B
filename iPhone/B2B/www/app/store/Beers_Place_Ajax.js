bcdb6ef6cc2fce2a15e6ed3caff7cbde2fabd9b9
morlins <gmorlini@imolinfo.it>
2012-09-07 18:19:47 +0200

    nuovo store beer-place

---
+++ iPhone/B2B/www/app/store/Beers_Place_Ajax.js
@@ -0,0 +1,39 @@
+Ext.define("B2B.store.Beers_Place_Ajax", {
+    extend: "Ext.data.Store",
+    requires: "Ext.data.proxy.JsonP",
+    id: "Beers_Ajax",
+    config: {
+        model: "B2B.model.Beer",
+        proxy: {
+            // Mock for Development
+            type:'ajax',
+            url:'json/mock_beerlist.json',
+            type: (HH.OFFLINE_MODE ? 'ajax' : 'jsonp'),
+            url: (HH.OFFLINE_MODE ? 'json/mock_beerlist.json' : HH.IP_PORT_SERVER + '/birrettaservice/rest/bserv/listBeerByPlace_jsonp'),
+            reader: {
+                type:'json',
+                rootProperty: 'response.body.list',
+                successProperty: 'response.status.success',
+                totalProperty: 'response.status.count',
+                messageProperty: 'response.status.msg'
+            },/*,
+            extraParams:{
+                idPlace:'4cb0a4e3ef1b3704ea9a3c00'
+            }*/  
+        },
+        autoLoad: true,
+        listeners:{
+            exception:function(proxy, response, orientation){
+                console.error('Failure Notification', response.responseText);
+                utils.alert('Loading failed', response.statusText);
+            },
+            callback: function(success,response){
+                console.log("Beers Store Callback");
+            },
+            load:function(el,records, successful){
+                HH.log("* Loaded: Store.Beer_Ajax");
+            }
+        }
+    }
+
+});
